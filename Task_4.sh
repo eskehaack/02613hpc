bd39334904dfaae9010676586dd160f8d63b2ebe
jukobo <s214704@dtu.com>
2026-04-15 10:14:39 +0200

    Update, task 4

---
+++ Task_4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#BSUB -J jacobi_profile
+#BSUB -q hpc
+
+#BSUB -n 8
+#BSUB -R "span[hosts=1]"
+#BSUB -W 01:00
+
+#BSUB -o jacobi_profile.out
+#BSUB -e jacobi_profile.err
+
+#BSUB -R "rusage[mem=16GB]"
+#BSUB -M 16GB
+
+#BSUB -R "select[model == XeonGold6226R]"
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613_2026
+
+python -m kernprof -l -v simulate_4.py 10
