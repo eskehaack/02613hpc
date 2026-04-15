37378b87f783de911c3fa4edfacc80bac39c35ca
jukobo <s214704@dtu.com>
2026-04-15 10:32:08 +0200

    Task 9

--- Task_9.sh
+++ Task_9.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#BSUB -J jacobi_profile
+#BSUB -J CuPy_solution
 #BSUB -q hpc
 
 #BSUB -n 8
 #BSUB -R "span[hosts=1]"
 #BSUB -W 01:00
 
-#BSUB -o jacobi_profile.out
-#BSUB -e jacobi_profile.err
+#BSUB -o CuPy_solution.out
+#BSUB -e CuPy_solution.err
 
 #BSUB -R "rusage[mem=16GB]"
 #BSUB -M 16GB
@@ -17,6 +17,12 @@
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613_2026
 
-# Run for a small subset of floorplans {10, 20, 40} to compare performance with reference solution, measure and print runtime and memory usage.
-python -m kernprof -l -v Task_9.py 10
+# Run for a small subset of floorplans and time the execution
+for n in 10 20 40
+do
+    echo "======================================"
+    echo "Running with n_floorplans = $n"
+    echo "======================================"
 
+    /usr/bin/time -v python -m kernprof -l -v Task_9.py $n
+done
