a17343c16be08b93736523a49da51ada9a4a7a01
Eske Haack <eskehaack@gmail.com>
2026-04-15 10:33:52 +0200

    kernels are not allowed to be device functions

--- Task_8.py
+++ Task_8.py
@@ -13,16 +13,21 @@ def load_data(load_dir, bid):
     interior_mask = np.load(join(load_dir, f"{bid}_interior.npy"))
     return u, interior_mask
 
-@cuda.jit(device=True)
+
+@cuda.jit()
 def jacobi_kernel(interior_mask, u, u_new):
     # single iteration of jacobi method, to be called from GPU
     i = cuda.grid(1)
     if i < len(interior_mask):
         idx = interior_mask[i]
-        u_new[idx] = 0.25 * (u[idx[0], idx[1] - 1] + u[idx[0], idx[1] + 1] + u[idx[0] - 1, idx[1]] + u[idx[0] + 1, idx[1]])
+        u_new[idx] = 0.25 * (
+            u[idx[0], idx[1] - 1]
+            + u[idx[0], idx[1] + 1]
+            + u[idx[0] - 1, idx[1]]
+            + u[idx[0] + 1, idx[1]]
+        )
         u[idx] = u_new[idx]
     return u
-
 
 
 def jacobi_cuda(u, interior_mask, max_iter):
@@ -42,17 +47,17 @@ def summary_stats(u, interior_mask):
     pct_above_18 = np.sum(u_interior > 18) / u_interior.size * 100
     pct_below_15 = np.sum(u_interior < 15) / u_interior.size * 100
     return {
-        'mean_temp': mean_temp,
-        'std_temp': std_temp,
-        'pct_above_18': pct_above_18,
-        'pct_below_15': pct_below_15,
+        "mean_temp": mean_temp,
+        "std_temp": std_temp,
+        "pct_above_18": pct_above_18,
+        "pct_below_15": pct_below_15,
     }
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Load data
-    LOAD_DIR = '/dtu/projects/02613_2025/data/modified_swiss_dwellings/'
-    with open(join(LOAD_DIR, 'building_ids.txt'), 'r') as f:
+    LOAD_DIR = "/dtu/projects/02613_2025/data/modified_swiss_dwellings/"
+    with open(join(LOAD_DIR, "building_ids.txt"), "r") as f:
         building_ids = f.read().splitlines()
 
     if len(sys.argv) < 2:
@@ -65,7 +70,7 @@ if __name__ == '__main__':
 
     # Load floor plans
     all_u0 = np.empty((N, 514, 514))
-    all_interior_mask = np.empty((N, 512, 512), dtype='bool')
+    all_interior_mask = np.empty((N, 512, 512), dtype="bool")
     for i, bid in enumerate(building_ids):
         u0, interior_mask = load_data(LOAD_DIR, bid)
         all_u0[i] = u0
@@ -84,8 +89,8 @@ if __name__ == '__main__':
     elapsed = end_time - start_time
 
     # Print summary statistics in CSV format
-    stat_keys = ['mean_temp', 'std_temp', 'pct_above_18', 'pct_below_15']
-    print('building_id, ' + ', '.join(stat_keys))  # CSV header
+    stat_keys = ["mean_temp", "std_temp", "pct_above_18", "pct_below_15"]
+    print("building_id, " + ", ".join(stat_keys))  # CSV header
     for bid, u, interior_mask in zip(building_ids, all_u, all_interior_mask):
         stats = summary_stats(u, interior_mask)
         print(f"{bid},", ", ".join(str(stats[k]) for k in stat_keys))
